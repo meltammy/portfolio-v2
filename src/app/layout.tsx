0c37587d0e5ba096a0c5e96280d0d3a0dc027406
meltammy <mel.tammy2@gmail.com>
2025-08-30 21:51:51 -0300

    feat: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,13 +6,20 @@ const montserrat = Montserrat({
   variable: "--font-montserrat",
   subsets: ["latin"],
   display: "swap",
-  weight: ["200", "400", "500", "600", "700"], // ajuste conforme necessário
+  weight: ["200", "400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
   title: "Melissa Tammy | Desenvolvedora Frontend e Full Stack",
   description:
     "Conheça os projetos de Melissa Tammy, desenvolvedora web com atuação em frontend e backend, criando interfaces eficientes e aplicações escaláveis.",
+  robots: {
+    index: true,
+    follow: true,
+  },
+  alternates: {
+    canonical: "https://www.meltammy.com.br",
+  },
 };
 
 export default function RootLayout({
